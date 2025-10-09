86859a90d9aeac38e495f957176632144fba7ae9
JVCA101 <joaovictordealmeid@gmail.com>
2025-10-09 11:08:32 -0300

    add comeco do trabalho 1

---
+++ Trabalho 01/frag.glsl
@@ -0,0 +1,22 @@
+uniform float time;
+
+varying vec3 fragPos;
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main()
+{
+	float r = 1.0 - length(fragPos.xyz - vec3(0.5, 0.25, 0.0));
+	float g = 1.0 - length(fragPos.xyz - vec3(-0.5, -0.25, 0.0));
+    float b = 1.0 - length(fragPos.xyz - vec3(0.5, -0.25, 0.0));
+
+	// if(fragPos.x > 0.0)
+		gl_FragColor = vec4(hsv2rgb(vec3((r + b) + time, time, time)), 1.0);
+	// else
+		// gl_FragColor = vec4(vec3(r, g, b), 1.0);
+}
---
+++ Trabalho 01/script.js
@@ -0,0 +1,62 @@
+import * as THREE from "https://threejs.org/build/three.module.js";
+
+var camera, scene, renderer, mesh;
+var clock = new THREE.Clock();
+
+init();
+
+async function init()
+{
+	const vert = await (await fetch("vert.glsl")).text();
+	const frag = await (await fetch("frag.glsl")).text();
+
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+    const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+	const material = new THREE.ShaderMaterial(
+	{
+		uniforms:
+		{
+			time: { value: 0 }
+		},
+		vertexShader: vert,
+		fragmentShader: frag
+	});
+
+	mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z = -2.5;
+	scene.add(mesh);
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+  
+	renderer.setClearColor(0x888888, 1);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setAnimationLoop(animate);
+	document.body.appendChild(renderer.domElement);
+
+	window.addEventListener("resize", onWindowResize);
+
+}
+
+function onWindowResize()
+{
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate()
+{
+	mesh.material.uniforms.time.value += Math.sin(clock.getDelta() * 0.5);
+    
+	// if(mesh.material.uniforms.time.value >= 2.0)
+	// 	mesh.material.uniforms.time.value -= 1.0;
+
+    mesh.rotation.y += 0.005;
+	
+	renderer.render(scene, camera);
+}
